4f81c7a8673cfc112e51af12c6c0543fc7d00477
toxinu <toxinu@gmail.com>
2011-09-14 23:29:17 +0200

    Initial commit for new branch (refactoring_conf)

---
+++ resources/lib/conf.py
@@ -0,0 +1,37 @@
+class configuration:
+	"""Configuration file content"""
+	def read(self, config_file ='resources/pyhame.conf'):
+		import configparser, sys
+		config = configparser.RawConfigParser()
+		config.read(config_file)
+		section = "general"
+		try:
+			self.website_title           	= config.get(section, 'website_title')
+			self.content_folder          	= config.get(section, 'content_folder')
+			self.template_name           	= config.get(section, 'template_name')
+			self.website_url             	= config.get(section, 'website_url')
+			self.extensions_to_render    	= config.get(section, 'extensions_to_render')
+			self.no_list_no_render       	= config.get(section, 'no_list_no_render')
+			self.no_list_yes_render      	= config.get(section, 'no_list_yes_render')
+			self.tpl_path                	= config.get(section, 'tpl_path')
+			self.lib_path                	= config.get(section, 'lib_path')
+			self.static_path             	= config.get(section, 'static_path')
+		except configparser.Error as err:
+			print('There is an error in pyhame.conf (%s)' % err)
+			sys.exit(1)
+		## Others ##
+		section = "others"
+		try:
+			self.archive                    = config.get(section, 'archive')
+		except configparser.Error as err:
+			print('There is an error in pyhame.conf (%s)' % err)
+			sys.exit(1)
+		## Remote ##
+		section = "remote"
+		try:
+			self.remote                     = config.get(section, 'remote')
+			self.remote_host                = config.get(section, 'remote_host')
+			self.remote_user         	= config.get(section, 'remote_user')
+			self.remote_path		= config.get(section, 'remote_path')
+		except configparser.Error as err:
+			print('There is an error in pyhame.conf (%s)' % err)
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import sys
+sys.path.append("resources")
+from lib.conf import configuration as conf
+conf.read(conf)
+
+#print(conf.website_title)
