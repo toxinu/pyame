815cc3f24141e852e6dcaa3b17cb46345cd59bec
toxinu <toxinu@gmail.com>
2011-09-16 16:59:46 +0200

    Remove useless file

--- resources/lib/build.py
+++
@@ -1,7 +0,0 @@
-def string_to_list(elements):
-	""" elements : string ("txt","md"...)"""
-	elements_list = []
-	for element in elements.split(','):
-		element = element.replace('"', '')
-		elements_list.append(element)
-	return elements_list
