6931f7447475cccd18d1ba5a3b9d3ee76ed62517
toxinu <toxinu@gmail.com>
2011-10-08 13:42:11 +0200

    It works !

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
 setup(
 	name="pyhame",
 	version="0.8.2",
 	author = "Socketubs",
-	author_email = "socketubs@gmail.com",
-	description = "Static website creator with Markdown syntax",
-	license = "GPL",
-	keywords = "website static markdown",
-	url = "http://pyhame.socketubs.net/",
-	scripts = ['bin/pyhame'],
+    author_email = "socketubs@gmail.com",
+    description = "Static website creator with Markdown syntax",
+    license = "GPL",
+    keywords = "website static markdown",
+    url = "http://pyhame.socketubs.net/",
 	packages = find_packages('src'),
-	package_dir = {'': 'src'},
-	package_data = {
-        'pyhame': ['data/*'],
-    }
+	package_dir = {'':'src'},
+	include_package_data=True,
+	scripts = ['bin/pyhame'],
+	install_requires=[
+		'setuptools',
+		'jinja2',
+	],
 )
