9d2ade32383247e1a0388775ba57f4b5c6805959
toxinu <toxinu@gmail.com>
2011-10-10 16:33:42 +0200

    Package uploaded on pypi.python.org

--- setup.py
+++ setup.py
@@ -8,16 +8,15 @@ if sys.version_info <= (3, 0):
 	exit(0)
 
 setup(
-	name="pyhame",
-	version="0.8.2",
+	name = 'pyhame',
+	version = '0.8.2',
 	author = "Socketubs",
     author_email = "socketubs@gmail.com",
     description = "Static website creator with Markdown syntax",
-	long_description=read('README'),
-    license = "GPL",
+    license = "AGPLv3",
     keywords = "website static markdown",
-    url = "http://pyhame.socketubs.net/",
-	include_package_data=True,
+	url = "http://pyhame.socketubs.net/",
+	include_package_data = True,
 	packages = find_packages('src'),
 	package_dir = {'':'src'},
 	scripts = ['bin/pyhame'],
