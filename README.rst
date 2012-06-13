443899a90f69ec52160efc53b080f4ecd5daa268
toxinu <toxinu@gmail.com>
2012-06-13 23:23:36 +0200

    Some clean

---
+++ README.rst
@@ -0,0 +1,36 @@
+=====
+Pyame
+=====
+
+An easy static website engine written in Python.  
+You just put files/folders in your content folder and launch pyame.  
+You don't need to do Html/Css/Php or other web language.
+
+Where is the magic ?
+--------------------
+
+Easy as: ::
+
+	pip-3.2 pyame
+	pyame create my_website
+	cd my_website
+	pyame serve
+
+And look at http://127.0.0.1:8080 in your web browser.
+
+**Good things for simple users** :
+
+- Markdown usage with highlight.js
+- Just create directories
+- Simple Html template
+- Very light static web site
+- Simple archive generator
+- Write with your preferred editor
+- Light and easy to use
+- Embedded web server
+- Auto new version detection
+
+**Others good things for advanced users** :
+
+- Jinja2 for easy templating
+- Python3
--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+import os
 import sys
-from setuptools import setup
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 if sys.version_info <= (3, 0):
     print("Pyame at least need Python 3.0.")
     exit(0)
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
 setup(
     name = 'pyame',
     version = '0.8.8',
-    author = "Socketubs",
+    author = "Geoffrey Lehée",
     author_email = "geoffrey@lehee.name",
     description = "Static website creator with Markdown syntax",
-    license = "No Licence",
+    license = open("LICENSE").read(),
     keywords = "website static markdown html",
     url = "https://github.com/Socketubs/Pyame",
     include_package_data = True,
     packages = ['pyame'],
     scripts = ['bin/pyame'],
-    install_requires=['pip', 'Markdown', 'Jinja2', 'clint']
+    install_requires=['Markdown', 'Jinja2', 'clint']
 )
